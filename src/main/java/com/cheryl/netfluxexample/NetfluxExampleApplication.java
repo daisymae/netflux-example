b6470a5d41e0903d3747fa64ebef0fdb9efad071
daisymae <daisymae@orcadiafarm.com>
2018-05-20 15:32:25 -0500

    Bootstrap
    Added bootstrap and also moved to intelliJ. This one still doesn't run there.

--- src/main/java/com/cheryl/netfluxexample/NetfluxExampleApplication.java
+++ src/main/java/com/cheryl/netfluxexample/NetfluxExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NetfluxExampleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NetfluxExampleApplication.class, args);
 	}
 }
